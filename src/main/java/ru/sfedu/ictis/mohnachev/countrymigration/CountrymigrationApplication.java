c876dd3b7b3508a0f651c5e38a4fe2ca150b1820
Fullstack Developer <fullstack.dev@computools.com>
2023-01-22 17:52:03 +0100

    Создание приложения из генератора

---
+++ src/main/java/ru/sfedu/ictis/mohnachev/countrymigration/CountrymigrationApplication.java
@@ -0,0 +1,13 @@
+package ru.sfedu.ictis.mohnachev.countrymigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrymigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountrymigrationApplication.class, args);
+	}
+
+}
